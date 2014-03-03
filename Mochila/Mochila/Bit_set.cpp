5a61eb1af314aa841f58edb83dfde4f24dac1198
alu0100764666 <alu0100764666@ull.edu.es>
2014-03-03 17:15:39 +0000

    Bit set terminado

--- Mochila/Mochila/Bit_set.cpp
+++ Mochila/Mochila/Bit_set.cpp
@@ -1,32 +1,79 @@
 #include "Bit_set.h"
 
 
-Bit_set::Bit_set(void) : block_(0) {}
+Bit_set::Bit_set(void) {}
 
-Bit_set::Bit_set(block_t block) : block_(block) {}
+Bit_set::Bit_set(size_t size)  {
 
-Bit_set :: ~Bit_set(void) {}
+	int x;
+
+	if (size <= 0)
+		throw 2;
+	else {
+		size_ = size;
+		x = size / (sizeof(block_t)* 8);
+		block_.resize(x + 1);
+		for (int i = 0; i < block_.size(); i++)
+			block_[i] = 0;
+	}
+
+}
+
+Bit_set :: ~Bit_set(void) { block_.clear(); }
+
+void Bit_set::resize(size_t size) {
+	block_.clear();
+
+	int x;
+
+	if (size <= 0)
+		throw 2;
+	else {
+		size_ = size;
+		x = size / (sizeof(block_t)* 8);
+		block_.resize(x + 1);
+		for (int i = 0; i < block_.size(); i++)
+			block_[i] = 0;
+	}
+
+}
 
 void Bit_set::insertar(int q) {
 	block_t aux = 1;
+	int x;
+	int pos;
+	if (q > size_)
+		throw 3;
+	else {
+		x = q / (sizeof(block_t)* 8);
+		pos = q % (sizeof(block_t)* 8);
 
-	aux = aux << (q);
-	block_ = block_ | aux;
+		aux = aux << (pos);
+		block_[x] = block_[x] | aux;
+	}
 }
 
 void Bit_set::remover(int q) {
 	block_t aux = 1;
+	int x;
+	int pos;
+	if (q > size_)
+		throw 3;
+	else {
+		x = q / (sizeof(block_t)* 8);
+		pos = q % (sizeof(block_t)* 8);
 
-	aux = aux << (q);
-	aux = ~aux;
+		aux = aux << (pos);
+		aux = ~aux;
 
-	block_ = block_ & aux;
+		block_[x] = block_[x] & aux;
+	}
 }
 
-block_t Bit_set::get_block(void) { return block_; }
+block_t Bit_set::get_block(int x) { return block_[x]; }
 
-void Bit_set::set_block(block_t Q) { block_ = Q; }
-
+void Bit_set::set_block(block_t Q, int x) { block_[x] = Q; }
+/*
 int Bit_set::pop_ultimo(void) {
 
 	int i = 0;
@@ -45,32 +92,38 @@ int Bit_set::pop_ultimo(void) {
 	return i;
 
 
-}
+}*/
 
 int Bit_set::cardinal(void)  {
 
 	int i = 0;
 	block_t aux;
-	aux = block_;
 	block_t dummy = 1;
 
-	while (aux != 0) {
-		if ((aux & dummy) != 0)
-			i++;
-		aux = aux >> (1);
+	for (int j = 0; j < block_.size(); j++){
+		aux = block_[j];
+		while (aux != 0) {
+			if ((aux & dummy) != 0)
+				i++;
+			aux = aux >> (1);
 
+		}
 	}
-
 	return i;
 
 }
 
 bool Bit_set::estado(int i) {
+	
 	block_t aux = 1;
 	bool e;
+	int x, pos;
 
-	aux = aux << (i);
-	aux = aux & block_;
+	x = i / (sizeof(block_t)* 8);
+	pos = i % (sizeof(block_t)* 8);
+
+	aux = aux << (pos);
+	aux = aux & block_[x];
 
 	if (aux != 0)
 		e = true;
--- Mochila/Mochila/Bit_set.h
+++ Mochila/Mochila/Bit_set.h
@@ -1,20 +1,26 @@
 #pragma once
+#include <iostream>
+#include <vector>
 
+using namespace std;
 
 typedef unsigned long  block_t;
+typedef unsigned size_t;
 
 class Bit_set
 {
-	block_t block_;//Conjunto
+	size_t size_;
+	vector <block_t> block_;				//Conjunto
 public:
 	Bit_set(void);
-	Bit_set(block_t block);
+	Bit_set(size_t);
 	~Bit_set(void);
+	void resize(size_t);
 	void insertar(int q);
 	void remover(int q);
-	block_t get_block(void);
-	void set_block(block_t Q);
-	int pop_ultimo(void);
+	block_t get_block(int);
+	void set_block(block_t, int);
+	//int pop_ultimo(void);
 	int cardinal(void);
 	bool estado(int i);//true si "i" contenido en block_
 };
